4893d3c7017f2acfd46cd589c8db422034c38ee7
jimmy <jimmyjaytscheina@gmail.com>
2020-07-01 11:45:09 +0200

    X is bigger than Y p.e.

--- src/A2.java
+++ src/A2.java
@@ -1,17 +1,17 @@
 public class A2 {
 
     public static void main(String[] args) {        //main method
-        double x = 10;                            // double to be able to fill in int or float Numbers
+        double x = 0;                               // double to be able to fill in int or float Numbers
 
-        if ( x > 0) {                                // if x is above 0
+        if ( x > 0) {                               // if x is above 0
 
             if (x>1000000){
-                System.out.println("Is a large positive Number"); //if it is above 0 and 1000000
+                System.out.println("Is a large positive Number");     //if it is above 0 and 1000000
             }
             if (x<1) {
                     System.out.println("Is a small positive Number"); //if it is above 0 but below 1
                 //
-         } else {
+            } else {
                 System.out.println("Is a positive Number");           //only positive, above 1 but below 1 000 000
            }
 
