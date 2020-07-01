4d59f7b9bfd9668cb4df790aeb98ad28382bdf7b
jimmy <jimmyjaytscheina@gmail.com>
2020-07-01 13:06:19 +0200

    X is bigger than Y p.e.

---
+++ src/A4.java
@@ -0,0 +1,18 @@
+public class A4 {
+
+
+    public static void main(String[] args) {
+        int x = 13;
+        int y = 20;
+
+        if (x>y) {
+            System.out.println("X is bigger than Y");
+        } else if (x<y){
+            System.out.println("Y is bigger than X");
+        } else if (x == y) {
+            System.out.println("X and Y are equal");
+        }
+
+    }
+
+}
