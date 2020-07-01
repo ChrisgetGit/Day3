d0da8701ae6986dd170b620327aaddd836a0f792
jimmy <jimmyjaytscheina@gmail.com>
2020-07-01 14:19:49 +0200

    multiply able with 3 or 5 (FUZZ/BUZZ) or both (FIZZBUZZ) or just the number

--- src/A6.java
+++ src/A6.java
@@ -1,15 +1,15 @@
 public class A6 {
     public static void multiplyAble(int n) {
         if (n % 3 == 0 && n % 5 ==0) {
-            System.out.println("FIZZBUZZ!");                    //returns FIZZBUZZ if multiply able with 3 AND 5
+            System.out.println("FIZZBUZZ!");                    //returns FIZZBUZZ if multiplier of 3 AND 5
         } else if (n % 5 == 0) {
-            System.out.println("BUZZ!");                        // returns BUZZ if multiply able with 5
+            System.out.println("BUZZ!");                        // returns BUZZ if multiplier of 5
 
         } else if  (n % 3 == 0){
-            System.out.println("FUZZ!");                        // returns FUZZ if multiply able with 3
+            System.out.println("FUZZ!");                        // returns FUZZ if multiplier of 3
 
         } else {
-            System.out.println(n + " is not multiply able by 3 or 5!"); //returns non-multiply able n and text
+            System.out.println(n + " is not a multiplier of 3 or 5!"); //returns not a multiplier of 3 or 5
         }
 
 
@@ -18,7 +18,7 @@ public class A6 {
     public static void main(String[] args)      //mainMethod
     {
         int n = 13;                             //put number in here
-        multiplyAble(n);                        //executing the method
+        multiplyAble(n);                        //execute the method
     }
 }
 
