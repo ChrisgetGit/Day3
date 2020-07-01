d44f231e68361d3eda1e087eaee79d7292ab2a76
jimmy <jimmyjaytscheina@gmail.com>
2020-07-01 10:14:15 +0200

    positive, large positive, small positive, zero or negative

--- src/A2.java
+++ src/A2.java
@@ -1,25 +1,28 @@
 public class A2 {
 
     public static void main(String[] args) {        //main method
-        int x = -1000000;                                 // integer variable x
+        double x = 10;                            // double to be able to fill in int or float Numbers
 
         if ( x > 0) {                                // if x is above 0
 
             if (x>1000000){
-                System.out.println("Is a large positive Number");
-
-                if (x<1) {
-                    System.out.println("Is a small positive Number");
-                }
-            } else {
-                    System.out.println("Is a positive Number");
+                System.out.println("Is a large positive Number"); //if it is above 0 and 1000000
+            }
+            if (x<1) {
+                    System.out.println("Is a small positive Number"); //if it is above 0 but below 1
+                //
+         } else {
+                System.out.println("Is a positive Number");           //only positive, above 1 but below 1 000 000
            }
+
+        } else if (x == 0) {
+            System.out.println ("Number is ZERO");
 
         }
 
 
         else {
-            System.out.println("Number is negative");//else (if not above 0) print
+            System.out.println("Number is negative");       //first if condition not met and else if condition not met
             }
 
 
@@ -27,5 +30,8 @@ public class A2 {
 
 }
 
+/*Console
+Number is Zero
 
+Process finished with exit code 0*/
 
