69969c89dc8f9b2f2c09bc1a53ba89c21241dc36
jimmy <jimmyjaytscheina@gmail.com>
2020-07-01 09:54:40 +0200

    Initial commit

---
+++ src/A1.java
@@ -0,0 +1,19 @@
+public class A1 {
+
+    public static void main(String[] args) {        //main method
+        int x = 25;                                 // integer variable x
+
+        if (x > 0) {                                // if x is above 0
+            System.out.println("Number is positive");// then print
+        } else {
+            System.out.println("Number is negative");//else (if not above 0) print
+        }
+
+
+    }
+
+}
+/*
+Number is positive
+Process finished with exit code 0
+*/
---
+++ src/A2.java
@@ -0,0 +1,31 @@
+public class A2 {
+
+    public static void main(String[] args) {        //main method
+        int x = -1000000;                                 // integer variable x
+
+        if ( x > 0) {                                // if x is above 0
+
+            if (x>1000000){
+                System.out.println("Is a large positive Number");
+
+                if (x<1) {
+                    System.out.println("Is a small positive Number");
+                }
+            } else {
+                    System.out.println("Is a positive Number");
+           }
+
+        }
+
+
+        else {
+            System.out.println("Number is negative");//else (if not above 0) print
+            }
+
+
+    }
+
+}
+
+
+
